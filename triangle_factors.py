864f4ac4deed87484ba70ed3a7f70d0296d1369b
vashistreddy <vashisteppalapally@gmail.com>
2017-09-28 21:29:50 -0500

    this is a message

--- triangle_factors.py
+++ triangle_factors.py
@@ -11,3 +11,77 @@ while divisor < 500:
       counter += 1
       #Here as well.
       #One of these comments is a lie.
+README.md  triangle_factors.py
+ieee-cs@ieee-cs-5:~/git_by_fire$ subl triangle_factors.py 
+ieee-cs@ieee-cs-5:~/git_by_fire$ nano README.md 
+ieee-cs@ieee-cs-5:~/git_by_fire$ nano README.md 
+ieee-cs@ieee-cs-5:~/git_by_fire$ subl README.md 
+ieee-cs@ieee-cs-5:~/git_by_fire$ ls
+README.md  triangle_factors.py
+ieee-cs@ieee-cs-5:~/git_by_fire$ which pythohn
+ieee-cs@ieee-cs-5:~/git_by_fire$ which python
+/home/ieee-cs/anaconda3/bin/python
+ieee-cs@ieee-cs-5:~/git_by_fire$ cd
+ieee-cs@ieee-cs-5:~$ python triangle_factors.py
+python: can't open file 'triangle_factors.py': [Errno 2] No such file or directory
+ieee-cs@ieee-cs-5:~$ python triangle_factors.py
+python: can't open file 'triangle_factors.py': [Errno 2] No such file or directory
+ieee-cs@ieee-cs-5:~$ ls
+anaconda3  Desktop    Downloads    ieee-cs-txst                         Music                      Pictures  Templates
+blockly    Documents  git_by_fire  jupyter-panda-matplotlib-demo.ipynb  NHL 2015-16-main-page.csv  Public    Videos
+ieee-cs@ieee-cs-5:~$ cd git_by_fire/
+ieee-cs@ieee-cs-5:~/git_by_fire$ LS
+The program 'LS' is currently not installed. You can install it by typing:
+sudo apt install sl
+ieee-cs@ieee-cs-5:~/git_by_fire$ LS
+The program 'LS' is currently not installed. You can install it by typing:
+sudo apt install sl
+ieee-cs@ieee-cs-5:~/git_by_fire$ ls
+README.md  triangle_factors.py
+ieee-cs@ieee-cs-5:~/git_by_fire$ python triangle_factors.py 
+Traceback (most recent call last):
+  File "triangle_factors.py", line 9, in <module>
+    divisors += 1
+NameError: name 'divisors' is not defined
+ieee-cs@ieee-cs-5:~/git_by_fire$ cs
+The program 'cs' is currently not installed. You can install it by typing:
+sudo apt install csound
+ieee-cs@ieee-cs-5:~/git_by_fire$ ls
+README.md  triangle_factors.py
+ieee-cs@ieee-cs-5:~/git_by_fire$ ls
+README.md  triangle_factors.py
+ieee-cs@ieee-cs-5:~/git_by_fire$ ls
+README.md  triangle_factors.py
+ieee-cs@ieee-cs-5:~/git_by_fire$ cd ..
+ieee-cs@ieee-cs-5:~$ ls
+anaconda3  Desktop    Downloads    ieee-cs-txst                         Music                      Pictures  Templates
+blockly    Documents  git_by_fire  jupyter-panda-matplotlib-demo.ipynb  NHL 2015-16-main-page.csv  Public    Videos
+ieee-cs@ieee-cs-5:~$ cd Desktop/
+ieee-cs@ieee-cs-5:~/Desktop$ ls
+triangle_factors.py
+ieee-cs@ieee-cs-5:~/Desktop$ python triangle_factors.py 
+  File "triangle_factors.py", line 12
+    if divisors % counter = 0  
+                          ^
+SyntaxError: invalid syntax
+ieee-cs@ieee-cs-5:~/Desktop$ python triangle_factors.py 
+  File "triangle_factors.py", line 12
+    if divisors % 3 = 0  
+                    ^
+SyntaxError: invalid syntax
+ieee-cs@ieee-cs-5:~/Desktop$ clr
+No command 'clr' found, did you mean:
+ Command 'cyr' from package 'console-cyrillic' (universe)
+ Command 'cli' from package 'mono-runtime' (main)
+ Command 'clm' from package 'mcl' (universe)
+ Command 'csr' from package 'rheolef' (universe)
+ Command 'cl' from package 'cl-launch' (universe)
+ Command 'mlr' from package 'miller' (universe)
+ Command 'clp' from package 'coinor-clp' (universe)
+ Command 'car' from package 'ucommon-utils' (universe)
+clr: command not found
+ieee-cs@ieee-cs-5:~/Desktop$ clear
+[3;J
+ieee-cs@ieee-cs-5:~/Desktop$ git status
+fatal: Not a git repository (or any of the parent directories): .git
+ieee-cs@ieee-cs-5:~/Desktop$  git add -
